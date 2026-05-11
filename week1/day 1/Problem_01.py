b51c0de6e9159dd84a95f4fd6934f0f4620c7d6e
gsksd <gsksd64@gmail.com>
2026-05-11 17:31:51 +0530

    pens problem

---
+++ week1/day 1/Problem_01.py
@@ -0,0 +1,6 @@
+"""
+A shop sells pens for ₹5 each. Take quantity as input from the user. Print the total cost using an f-string.
+"""
+quantity = int(input("enter how many pens uh took: "))
+cost = 5 * quantity
+print(f"for {quantity} pens total cost will be {cost}")
