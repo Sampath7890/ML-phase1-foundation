3b87652829892186f738cbff349fe2782222e842
gsksd <gsksd64@gmail.com>
2026-05-14 11:34:24 +0530

    functions practise 3

---
+++ week1/day 4/functions_03.py
@@ -0,0 +1,9 @@
+def happy_birthday(name , age):
+    print(f"happy birthday to {name}")
+    print(f"you are {age} year's old")
+    print("happy happy birthday to uh")
+    print()
+
+happy_birthday("sam",19)
+happy_birthday("ram",20)
+happy_birthday("hanu",21)
