6bc3d933248964972118b8d7fd6e46068abac40c
gsksd <gsksd64@gmail.com>
2026-05-14 12:33:31 +0530

    def calculator

---
+++ week1/day 4/functions_05.py
@@ -0,0 +1,28 @@
+def add(x,y) :
+    z = x + y 
+    return z
+
+def sub(x,y) :
+    z = x - y 
+    return z
+
+def mul(x,y) :
+    z = x * y 
+    return z
+
+def div(x,y) :
+    z = x / y 
+    return z
+
+x =int(input("enter x value: "))
+y =int(input("enter y value: "))
+
+print(f"addition of {x} and y is {y} :",add(x,y))
+print(f"subtraction of {x} and y is {y} :",sub(x,y))
+print(f"multiplication of {x} and y is {y} :",mul(x,y))
+print(f"division of {x} and y is {y} :",div(x,y))
+
+
+
+
+
