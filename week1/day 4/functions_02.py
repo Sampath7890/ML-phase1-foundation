73f247236560f84008bc800eac2f6ceaab3a3884
gsksd <gsksd64@gmail.com>
2026-05-14 11:25:24 +0530

    defhappy birthday

---
+++ week1/day 4/functions_02.py
@@ -0,0 +1,9 @@
+def happy_birthday():
+    print("happy birthdat to uh")
+    print("many long days to uh")
+    print("happy happy birthday to uh")
+count = 0
+for i in range(5) :
+    happy_birthday()
+    count+=1
+    print(count)
