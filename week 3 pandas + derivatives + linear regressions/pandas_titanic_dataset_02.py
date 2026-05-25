abed810f089d1a5df27077443c0616a62e5e70f0
gsksd <gsksd64@gmail.com>
2026-05-25 21:43:23 +0530

    exploring titanic dataset using pandas

---
+++ week 3 pandas + derivatives + linear regressions/pandas_titanic_dataset_02.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df = pd.read_csv('tested.csv')
+print(df["Age"].head())
+print(df[["Name", "Age", "Survived"]].head())
+print(df.iloc[0])
+print(df.iloc[1:5].to_string())
+print(df.iloc[-1])
