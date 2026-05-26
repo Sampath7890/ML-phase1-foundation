bf41efeb3e081c97e2526673b9c0790a22c32f60
gsksd <gsksd64@gmail.com>
2026-05-26 18:21:29 +0530

    basic states

---
+++ week 3 pandas + derivatives + linear regressions/day 2/basic_states.py
@@ -0,0 +1,17 @@
+"""P3 Easy
+Basic stats
+Answer using Pandas — one line each:
+How many passengers survived?
+How many did not survive?
+What was the average age of all passengers?
+What was the most expensive ticket fare?"""
+
+import pandas as pd
+df = pd.read_csv(r'C:\ML phase 1\week 3 pandas + derivatives + linear regressions\day 1\tested.csv')
+print("Survived : ",df['Survived'].sum())
+print("not survived :",(df['Survived'] == 0).sum())
+print("Average aage :",df["Age"].mean())
+print("youngest aage :",df["Age"].min())
+print("oldest aage :",df["Age"].max())
+print("most expensive fare:", df["Fare"].max())
+print(df.loc[df['Fare'].idxmax()])
