f75084e6830b586fb390c7a5e304eabc27aff8bf
gsksd <gsksd64@gmail.com>
2026-05-26 18:53:18 +0530

    missing values

---
+++ week 3 pandas + derivatives + linear regressions/day 2/missing_values.py
@@ -0,0 +1,19 @@
+"""P5 Medium
+Missing values
+How many missing values does Age have?
+Fill missing Age values with the median age.
+How many missing values does Age have now?
+Drop any rows where Embarked is missing.
+Print the shape before and after each operation."""
+import pandas as pd
+df = pd.read_csv(r'C:\ML phase 1\week 3 pandas + derivatives + linear regressions\day 1/tested.csv')
+print("original shape: ",df.shape)
+print("missing values Before operantions \n",df.isnull().sum())
+print("missing values of Age",df['Age'].isnull().sum())
+print("Shape before fillna ",df.shape)
+df['Age'] = df['Age'].fillna(df['Age'].median())
+print("Shape after fillna ",df.shape)
+print("\nMissing Age values after fillna:",
+      df['Age'].isnull().sum())
+print("\nMissing values after operations:")
+print(df.isnull().sum())
