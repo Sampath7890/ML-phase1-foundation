55e66f7e6d531e67fdb23957460362b21663dc15
gsksd <gsksd64@gmail.com>
2026-05-26 17:59:09 +0530

    first look

---
+++ week 3 pandas + derivatives + linear regressions/day 2/selecting.py
@@ -0,0 +1,13 @@
+"""P2 Easy
+Selecting
+Print only the Name, Age, Sex and Survived columns.
+Print the 10th passenger (row index 9) — all their details.
+Print the last 3 passengers using iloc."""
+
+import pandas as pd
+df = pd.read_csv(r'C:\ML phase 1\week 3 pandas + derivatives + linear regressions\day 1\tested.csv')
+print(df[["Name",  "Age" , "Sex" , "Survived"]].to_string())
+print("10th passanger details : \n")
+print(df.iloc[9])
+print("last 3 passengers using iloc : \n")
+print(df.iloc[-3:])
